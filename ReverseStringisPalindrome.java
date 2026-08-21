188db1f3237d92ccc7f3ac752607cd1260539fc1
Kavinayasri J <kavinayasri02@gmail.com>
2026-08-21 12:12:24 +0530

    Add files via upload

---
+++ ReverseStringisPalindrome.java
@@ -0,0 +1,27 @@
+//Using two pointer method
+
+import java.util.*;
+public class ReverseStringisPalindrome {
+    public static void main(String[]args){
+        Scanner kavi = new Scanner(System.in);
+        //Enter a String to check Palindrome
+        String k = kavi.nextLine();
+        String str = k.toLowerCase();
+        int left = 0;
+        int right = str.length()-1;
+        boolean isPalindrome = true;
+        while(left<right){
+            if(str.charAt(left) != str.charAt(right)){
+                isPalindrome = false;
+                break;
+            }
+            left++;
+            right--;
+        }
+        if(isPalindrome){
+            System.out.println("'" + k + " is a Palindrome");
+        }else{
+            System.out.println("'" + k + " is not a Palindrome");
+        }
+    }
+}
